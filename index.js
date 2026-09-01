de51c9be6691ead20eb3960b88d7780923b9f177
zain1234khan <zain.sunztech7@gmail.com>
2026-09-01 14:11:07 +0500

    nothing

--- index.js
+++ index.js
@@ -1,59 +1,24 @@
-const http = require("http");
-const fs = require("fs");
-const url = require("url");
+const http = require("http");   
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+
+   return res.send("Hello from about page");
+
+});
+
+
+app.get("/about", (req, res) => {
+
+   return res.send("Hello from about page" + 'hey' + ''+  req.query.name +''+ 'you are ' + req.query.age + ' years old');
+
+});
 
 function myhandler(req, res) {
 
-    if (req.url === "/favicon.ico") return res.end();
-
-    const log = `${Date.now()}: ${req.method} ${req.url} new request received\n`;
-
-    const myUrl = url.parse(req.url, true);
-
-    // Save request to log.txt
-    fs.appendFile("log.txt", log, (err) => {
-        if (err) {
-            console.log("Error writing log:", err);
-        }
-    });
-
-    switch (myUrl.pathname) {
-
-        case "/":
-            if (req.method === "GET") {
-                res.end("homepage");
-            } else {
-                res.end("Method Not Allowed");
-            }
-            break;
-
-        case "/about":
-            const username = myUrl.query.myname;
-            res.end(`Hi, ${username}`);
-            break;
-
-        case "/search":
-            const search = myUrl.query.search_query;
-            res.end("Here are your results for " + search);
-            break;
-
-        case "/signup":
-            if (req.method === "GET") {
-                res.end("this is a signup form");
-            } else if (req.method === "POST") {
-                // DB query
-                res.end("success");
-            } else {
-                res.end("Method Not Allowed");
-            }
-            break;
-
-        default:
-            res.end("404 Not Found");
-    }
-    
 }
-const myserver = http.createServer(myhandler);
+const myserver = http.createServer(app);
 
 myserver.listen(8000, () => {
     console.log("Server started!");
